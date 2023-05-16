9050cda27380d8c523f564534d8d8f7ea1356624
Tinotenda Mawaro <tmawaro25@gmail.com>
2023-05-16 11:07:31 -0400

    add react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
-import './App.css';
-import ExamRoom from './components/ExamRoom';
+import React, { useState } from "react";
+import "./App.css";
+import ExamRoom from "./components/ExamRoom";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   const [examRoomState] = useState({});
 
   return (
-    <div className="App">
-      <ExamRoom />
-
+    <div className="App relative h-screen ">
+      <Routes className="flex grow">
+        <Route path="/login" element={<Login />} />
+        <Route path="/exam" element={<ExamRoom />} />
+      </Routes>
+      <div className="absolute bottom-5 flex text-gray-500 w-full justify-center">
+        <p className="">Copyright (c) 2023 Optic Inc.AllRights Reserved.</p>
+      </div>
     </div>
   );
 }
