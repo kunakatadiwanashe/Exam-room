7d0bed8b750a136d29ed8d0be09e1b2e37325dae
Tinotenda Mawaro <tmawaro25@gmail.com>
2023-05-18 05:16:37 -0400

    save to allow merge from main

--- frontend/src/components/ExamInstructions/Instructions.js
+++ frontend/src/components/ExamInstructions/Instructions.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Instructions = () => {
   return (
-    <div className='instructions w-[100%] rounded-lg rounded-br border p-8   bg-[#D6D6D6]'>
-        <h3 className='kun '>INSTRUCTIONS TO CANDIDATE:</h3>
-        <ol className='list-decimal pt-21'>
+    <div className='instructions w-[100%] rounded-lg rounded-br border p-8 border-blue-500  bg-gray-700'>
+        <h3 className='kun text-blue-500'>INSTRUCTIONS TO CANDIDATE:</h3>
+        <ol className='list-decimal pt-21 text-white'>
             <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</li>
             <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</li>
             <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</li>
